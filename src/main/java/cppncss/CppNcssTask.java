59e4508ae8de15d37177ba58538806db6d30c5f5
mat007 <mat007@0cf59fa1-751c-0410-8529-da8c0b0e53fa>
2006-12-20 19:45:16 +0000

    Ant task first draft
    git-svn-id: https://cppncss.svn.sourceforge.net/svnroot/cppncss/trunk@226 0cf59fa1-751c-0410-8529-da8c0b0e53fa

---
+++ src/main/java/cppncss/CppNcssTask.java
@@ -0,0 +1,104 @@
+/**
+ * Redistribution  and use  in source  and binary  forms, with  or without
+ * modification, are permitted provided  that the following conditions are
+ * met :
+ *
+ * . Redistributions  of  source  code  must  retain  the  above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * . Redistributions in  binary form  must reproduce  the above  copyright
+ *   notice, this list of conditions  and the following disclaimer in  the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * . The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS  PROVIDED BY THE  AUTHOR ``AS IS''  AND ANY EXPRESS  OR
+ * IMPLIED  WARRANTIES,  INCLUDING,  BUT   NOT  LIMITED  TO,  THE   IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND  FITNESS FOR A PARTICULAR  PURPOSE ARE
+ * DISCLAIMED.  IN NO  EVENT SHALL  THE AUTHOR  BE LIABLE  FOR ANY  DIRECT,
+ * INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL  DAMAGES
+ * (INCLUDING,  BUT  NOT LIMITED  TO,  PROCUREMENT OF  SUBSTITUTE  GOODS OR
+ * SERVICES;  LOSS  OF USE,  DATA,  OR PROFITS;  OR  BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED  AND ON  ANY THEORY  OF LIABILITY,  WHETHER IN  CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY  WAY  OUT OF  THE  USE OF  THIS  SOFTWARE, EVEN  IF  ADVISED OF  THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package cppncss;
+
+import java.io.File;
+import java.util.Vector;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.AntlibDefinition;
+import org.apache.tools.ant.types.FileSet;
+
+/**
+ * @author Mathieu Champlon
+ */
+public final class CppNcssTask extends AntlibDefinition
+{
+    private final Vector filesets = new Vector();
+    private String prefix;
+    private String filename;
+
+    /**
+     * Add a set of source files.
+     *
+     * @param fileset a set of source files.
+     */
+    public void addFileset( final FileSet fileset )
+    {
+        filesets.add( fileset );
+    }
+
+    public void setPrefix( final String prefix )
+    {
+        this.prefix = format( prefix );
+    }
+
+    public void setToFile( final String filename )
+    {
+        this.filename = filename;
+    }
+
+    private String format( final String path )
+    {
+        String result = path.replace( '/', File.separatorChar ).replace( '\\', File.separatorChar );
+        if( result.charAt( result.length() - 1 ) != File.separatorChar )
+            result += File.separatorChar;
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void execute()
+    {
+        if( filename == null )
+            throw new BuildException( "Missing 'tofile' attribute to specify output file name" );
+        CppNcss.main( buildArgs() );
+    }
+
+    private String[] buildArgs()
+    {
+        final Vector args = new Vector();
+        args.add( "-x" );
+        args.add( "-f=" + filename );
+        if( prefix != null )
+            args.add( "-p=" + prefix );
+        for( int j = 0; j < filesets.size(); ++j )
+        {
+            final FileSet set = (FileSet)filesets.elementAt( j );
+            final DirectoryScanner scanner = set.getDirectoryScanner( getProject() );
+            final String directory = format( scanner.getBasedir().toString() );
+            final String[] files = scanner.getIncludedFiles();
+            for( int i = 0; i < files.length; ++i )
+                args.add( directory + files[i] );
+        }
+        return (String[])args.toArray( new String[args.size()] );
+    }
+}
