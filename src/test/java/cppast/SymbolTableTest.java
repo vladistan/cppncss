512e95b88ab7c0ee42e90b0e624de3d3d3ab8eb8
mat007 <mat007@0cf59fa1-751c-0410-8529-da8c0b0e53fa>
2008-11-09 13:18:05 +0000

    Cleanup
    git-svn-id: https://cppncss.svn.sourceforge.net/svnroot/cppncss/trunk@364 0cf59fa1-751c-0410-8529-da8c0b0e53fa

--- src/test/java/cppast/SymbolTableTest.java
+++ src/test/java/cppast/SymbolTableTest.java
@@ -106,12 +106,6 @@ public class SymbolTableTest extends TestCase
         symbols.openScope( "my_scope3" );
         symbols.closeScopes();
         symbols.extend( "my_scope1::my_scope2" );
-        assertEquals( "my_scope1::my_scope2::my_scope3::Symbol", symbols.getCurrentScope()
-                .resolve( "my_scope3::Symbol" ) );
-    }
-
-    public void testTmp()
-    {
-        assertEquals( "", "::".substring( 2 ) );
+        assertEquals( "my_scope1::my_scope2::my_scope3::Symbol", symbols.getCurrentScope().resolve( "my_scope3::Symbol" ) );
     }
 }
